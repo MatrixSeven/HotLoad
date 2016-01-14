b31c2a66310b94490276a97d10b87c7145db0ce0
seven <985390927@qq.com>
2016-01-14 14:54:54 +0800

    Revert "1.0"
    This reverts commit 74625376ca18e9d22845e2a764b9fa4f28b36a72.

--- hotLoadClass/RefChange.java
+++
@@ -1,9 +0,0 @@
-package hotLoadClass;
-
-/**
- * @author seven
- *  类热加载后回调接口
- */
-public interface RefChange {
-	void ReLoadClass(Object j);
-}
--- hotLoadClass/Say.java
+++
@@ -1,9 +0,0 @@
-package hotLoadClass;
-
-/**
- * @author seven
- * 说话接口
- */
-public interface Say {
-	public void say();
-}
